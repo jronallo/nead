17bb081b2adbfb0aec7f3ad1f0f2e2006a1ec5b4
Jason Ronallo <jronallo@gmail.com>
2011-11-20 08:25:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nead::Application.config.secret_token = '96b72f0842b92cece258703348f1b118fd8151f0fcb92ec5bf39e055d7859ed81c29f539d20fa9b3c5da0c55b680d1c067d38a46050730568f3983b32d62cd0d'
